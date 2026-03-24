11522aaabc5a62f3bca24905117e2e9141e2f2d1
chamaling <lamin.bangura113@gmail.com>
2026-03-24 18:02:31 -0400

    refactor: added light/dark mode button instead of key toggle

--- src/RootLayout.tsx
+++ src/RootLayout.tsx
@@ -1,7 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 export function RootLayout() {
-  return <Outlet />;
+  return (
+    <>
+      <div className="fixed top-4 right-4 z-50">
+        <ThemeToggle />
+      </div>
+      <Outlet />
+    </>
+  );
 }
 
 export default RootLayout;
